df30e144fd0cf62fa4fb85e7b7aba92e339ea896
kirinho <lushukov04@gmail.com>
2025-02-14 11:14:18 +0200

    Updated GlobalExceptionHandler

--- src/main/java/com/liushukov/cloud_file/exception/GlobalExceptionHandler.java
+++ src/main/java/com/liushukov/cloud_file/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.liushukov.cloud_file.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +25,43 @@ public class GlobalExceptionHandler {
             );
             return ResponseEntity.status(HttpStatusCode.valueOf(401)).body(errorDetails);
         }
+
+        if (exception instanceof AccountStatusException) {
+            var errorDetails = new ErrorDetails(
+                    Instant.now(),
+                    "The account is locked",
+                    webRequest.getDescription(false)
+            );
+            return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(errorDetails);
+        }
+
+        if (exception instanceof AccessDeniedException) {
+            var errorDetails = new ErrorDetails(
+                    Instant.now(),
+                    "You are not authorized to access this resource",
+                    webRequest.getDescription(false)
+            );
+            return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(errorDetails);
+        }
+
+        if (exception instanceof SignatureException) {
+            var errorDetails = new ErrorDetails(
+                    Instant.now(),
+                    "The JWT signature is invalid",
+                    webRequest.getDescription(false)
+            );
+            return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(errorDetails);
+        }
+
+        if (exception instanceof ExpiredJwtException) {
+            var errorDetails = new ErrorDetails(
+                    Instant.now(),
+                    "The JWT has expired",
+                    webRequest.getDescription(false)
+            );
+            return ResponseEntity.status(HttpStatusCode.valueOf(403)).body(errorDetails);
+        }
+
         return ResponseEntity.status(HttpStatusCode.valueOf(500))
                 .body(new ErrorDetails(
                         Instant.now(),
